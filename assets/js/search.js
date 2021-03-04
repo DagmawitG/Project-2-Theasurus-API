a6293f9aea4c3445f60b4c50e3ed96ced5b046f8
luhana daniel <se.luhana.daniel@gmail.com>
2021-03-04 05:15:46 +0300

    create function for when search is clicked

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,3 +4,17 @@ const output=document.querySelector('#output');
 const trial=document.querySelector('#trial');
 const try1=document.querySelector('#try1');
 const try2=document.querySelector('#try2');
+
+search.addEventListener('click',prin);
+
+function prin(e){
+    
+    
+    
+    console.log("why");
+    
+   
+    
+        
+    }
+    
