7120ae7d1176c947fb7d67f7c1e2f62e3880f722
DagmawitG <se.dagmawit.getachew@gmail.com>
2021-03-04 04:54:55 +0300

    quiz app js file created

---
+++ assets/js/quizapp.js
@@ -0,0 +1,34 @@
+questionBank= [
+    {
+        question : `SYNONYM OF : a`,
+        option : [`b`,'c','s','s'],
+        answer : `t`
+    
+    },
+    {
+        question : `SYNONYM OF : a`,
+        option : [`b`,'c','s','s'],
+        answer : `t`
+    },
+    {
+        question : `SYNONYM OF : a`,
+        option : [`b`,'c','s','s'],
+        answer : `t`
+    },
+    {
+        question : `SYNONYM OF : a`,
+        option : [`b`,'c','s','s'],
+        answer : `t`
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
