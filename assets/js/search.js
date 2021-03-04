48ce4f32d13120c3529a2733b3fa20686147836d
luhana daniel <se.luhana.daniel@gmail.com>
2021-03-04 05:24:06 +0300

    display synonyms of content in html

--- assets/js/search.js
+++ assets/js/search.js
@@ -13,22 +13,23 @@ function prin(e){
     let content= input.value;
     console.log(content);
     output.innerHTML=content;
-    console.log("why");
     
-   
+    
+    const api_url=`https://www.dictionaryapi.com/api/v3/references/ithesaurus/json/${content}?key=f4202a73-2937-4fd6-b68e-2954ef0d7f04`
     async function getapi(url){
         const response=await fetch(url);
 
         var data= await response.json();
         console.log(data);
 
-        
+        //creating object and getting synonyms
         var getKeys1=Object.keys(data['0'])['0'];
         var getVal3 = data['1'][getKeys1].syns[0];
          //access short definition
       //  var getKeys1=Object.keys(data['0'])['0'];
       //  var getVal3 = data['1'][getKeys1].syns[0];
       try1.innerHTML=getVal3;
+      console.log(getVal3);
     }
       
     getapi(api_url);}
