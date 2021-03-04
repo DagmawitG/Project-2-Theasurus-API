00fa39bea77ea2f32539510d5d2622f252961ead
luhana daniel <se.luhana.daniel@gmail.com>
2021-03-04 05:13:11 +0300

    initialized variables

---
+++ assets/js/search.js
@@ -0,0 +1,6 @@
+const search=document.querySelector('#search');
+const input = document.querySelector('#inputword');
+const output=document.querySelector('#output');
+const trial=document.querySelector('#trial');
+const try1=document.querySelector('#try1');
+const try2=document.querySelector('#try2');
